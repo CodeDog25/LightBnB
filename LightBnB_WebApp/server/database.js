ee11de948beaf294f3ebf33b9d28ea5a7e0b734d
Sicily Ding <xishading@gmail.com>
2023-02-24 21:33:24 -0700

    refactor getAllReservations

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -77,7 +77,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const values = [guest_id, limit];
+  return pool
+    .query(`
+    SELECT reservations.*, properties.*, AVG(rating) as average_rating
+    FROM reservations
+    JOIN properties ON properties.id = reservations.property_id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1 
+    AND reservations.end_date < now()::date
+    GROUP BY reservations.id, properties.id
+    ORDER BY reservations.start_date
+    LIMIT $2;`, values)
+    .then((result) => result.rows)
+    .catch((err) => console.log("query error", err));
 }
 exports.getAllReservations = getAllReservations;
 
